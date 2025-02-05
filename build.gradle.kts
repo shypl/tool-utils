51562735497a8dd2d091e533bfa09c3af2a87772
Alexey Chuprin <a.chuprin@capjack.ru>
2025-02-05 11:25:03 +0300

    kotlin 2.1.10 gradle 8.12.1 Configure publishing Configure .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "2.1.0"
+	kotlin("jvm") version "2.1.10"
 	id("java-library")
 	id("maven-publish")
 	id("nebula.release") version "19.0.10"
@@ -13,11 +13,14 @@ kotlin {
 
 repositories {
 	mavenCentral()
-	mavenLocal()
+	maven("https://maven.pkg.github.com/shypl/packages").credentials {
+		username = ""
+		password = project.property("shypl.gpr.key") as String
+	}
 }
 
 dependencies {
-	implementation("org.shypl.tool:tool-lang:1.0.0-SNAPSHOT")
+	implementation("org.shypl.tool:tool-lang:1.0.0")
 	implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.2")
 	implementation(kotlin("reflect"))
 	testImplementation(kotlin("test"))
@@ -31,4 +34,12 @@ publishing {
 	publications.create<MavenPublication>("Library") {
 		from(components["java"])
 	}
+	repositories.maven("https://maven.pkg.github.com/shypl/packages").credentials {
+		username = project.property("shypl.gpr.user") as String
+		password = project.property("shypl.gpr.key") as String
+	}
+}
+
+tasks.release {
+	finalizedBy(tasks.publish)
 }
